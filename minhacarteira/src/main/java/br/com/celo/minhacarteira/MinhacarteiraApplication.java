cb7387b8d550c14a9f3567d43ff970a2f753bd8c
Marcelo Alves Ferreira <marceloalvesferreira265@gmail.com>
2021-01-30 00:11:02 -0300

    Projeto Carteira

---
+++ minhacarteira/src/main/java/br/com/celo/minhacarteira/MinhacarteiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.celo.minhacarteira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhacarteiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhacarteiraApplication.class, args);
+	}
+
+}
---
+++ minhacarteira/src/test/java/br/com/celo/minhacarteira/MinhacarteiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.celo.minhacarteira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhacarteiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
